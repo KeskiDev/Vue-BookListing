f6c50e79ae0795534d776862202bb4fe17979e77
KeskiDev <keskidev@gmail.com>
2022-04-21 15:17:49 -0600

    module 2 and 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books" :key="book.title">{{book.title}} : {{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -11,6 +14,11 @@
         data() {
             return {
                 title: "All Books",
+                books:[
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
             }
         },
     };
@@ -19,5 +27,9 @@
 <style>
     h1,h2{
         font-weight: normal;
+    }
+    ul{
+        list-style-type: none;
+        padding: 0;
     }
 </style>
